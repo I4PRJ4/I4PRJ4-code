34fc1c440190587fd51d98e209ef49c6a401eae5
Marius Hambro Bovbjerg <marius@hambro.eu>
2021-04-21 13:32:26 +0200

    Reworked the role/login system

---
+++ Projekt-StudieTips/Data/DbAdmin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+namespace Projekt_StudieTips.Data
+{
+    public class DbAdmin
+    {
+        public static void SeedUsers(UserManager<IdentityUser> userManager, ILogger log)
+        {
+            const string adminEmail = "Admin@localhost";
+            const string adminPassword = "Secret7/";
+
+            if (userManager.FindByNameAsync(adminEmail).Result == null)
+            {
+                log.LogWarning("Seeding the admin user");
+                var user = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail
+                };
+                IdentityResult result = userManager.CreateAsync
+                    (user, adminPassword).Result;
+                if (result.Succeeded)
+                {
+                    var adminClaim = new Claim("Admin", "ADMINISTRATOR");
+                    userManager.AddClaimAsync(user, adminClaim);
+                }
+            }
+        }
+    }
+}
---
+++ Projekt-StudieTips/Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Projekt_StudieTips.Data;
+using System.Linq;
+
+namespace Projekt_StudieTips
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("server=[::1],1433; User Id=SA; Password=password_123; database=StudieTipsDB; trusted_connection=false;"));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+
+                // Password Settings
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 4;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true;
+
+                // User settings
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+;:";
+                options.User.RequireUniqueEmail = false;
+
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(
+                    "IsAdmin",
+                    policyBuilder => policyBuilder
+                     .RequireClaim("Admin"));
+            });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            UserManager<IdentityUser> userManager, ILogger<Startup> log)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            DbAdmin.SeedUsers(userManager, log);
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
