03bf4055a9fff293b7355a34c34db28e4b705472
eengstroem <59916483+eengstroem@users.noreply.github.com>
2021-05-21 13:33:14 +0200

    Removed som messy stuff, added foreach to home page

---
+++ Projekt-StudieTips/Models/MultipleModelsSearchAndTip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projekt_StudieTips.Models;
+
+namespace Projekt_StudieTips.Models
+{
+        public class MultipleModelsSearchAndTip
+            {
+            public SearchDto SearchDto { get; set; }
+            public List<Tip> Tips { get; set; }
+            }
+    
+}
