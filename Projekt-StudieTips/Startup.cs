c39db94dde8fe5023df1ca693ac3ea4ec1f4954d
WannTheMan <jisskiss@gmail.com>
2021-05-26 14:00:29 +0200

    Fixed connectionstring (FU MARIUS)

--- Projekt-StudieTips/Startup.cs
+++ Projekt-StudieTips/Startup.cs
@@ -26,7 +26,7 @@ namespace Projekt_StudieTips
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer("server=[::1],1433; User Id = SA; Password=password_123; database =StudieTipsDB; trusted_connection = false;"));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddScoped<DegreeRepository>();
@@ -68,7 +68,7 @@ namespace Projekt_StudieTips
             services.AddControllersWithViews();
 
             services.AddDbContext<DatabaseContext>(options =>
-                    options.UseSqlServer(("server=[::1],1433; User Id = SA; Password=password_123; database =StudieTipsDB; trusted_connection = false;")));
+                    options.UseSqlServer(Configuration.GetConnectionString("AppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
