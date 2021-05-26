643e3d05ecb6d892920bedd47a9aa39cec1bb8b2
eengstroem <59916483+eengstroem@users.noreply.github.com>
2021-05-26 14:17:42 +0200

    adding test-project

---
+++ Projekt-StudieTips.Test.Unit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Projekt_StudieTips.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
