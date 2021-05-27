84ad4eeb5fb5ae7ed2f290cee47b4323cd1bd8b0
Jakob <jakobhkristensen@gmail.com>
2021-05-27 12:55:22 +0200

    Sidste tests til DegreeController tilføjet

--- Projekt-StudieTips.Test.Unit/Tests/DegreeControllerTest.cs
+++ Projekt-StudieTips.Test.Unit/Tests/DegreeControllerTest.cs
@@ -93,6 +93,114 @@ namespace Projekt_StudieTips.Test.Unit
 
         }*/
 
+        [Test]
+        public void Redirect_WithPlaintextRoutes_ReturnsRedirectsToCorrectPaths()
+        {
+
+            //Arrange
+            const string goTo = "Gå til";
+            const string edit = "Rediger";
+            const string delete = "Slet";
+
+            //Act
+            var goToResult = _uut.Redirect(null, goTo) as RedirectToActionResult;
+            var editResult = _uut.Redirect(null, edit) as RedirectToActionResult;
+            var deleteResult = _uut.Redirect(null, delete) as RedirectToActionResult;
+            //Assert
+            ;
+            Assert.AreEqual("Index", goToResult?.ActionName);
+            Assert.AreEqual("Edit", editResult?.ActionName);
+            Assert.AreEqual("Delete", deleteResult?.ActionName);
+        }
+
+        [Test]
+        public async Task Edit_WithNoSpecifiedId_Returns_NotFound()
+        {
+            //Arrange
+
+            //Act
+            var result = await _uut.Edit(id: null);
+            //Assert
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+
+        public async Task Edit_WithIdMismatch_Returns_NotFound()
+        {
+            //Arrange
+            var degree = new Degree() { DegreeId = 0 };
+            //Act
+            var result = await _uut.Edit(2, degree);
+            //Assert
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Edit_WithIdMatch_ReturnsRedirectToIndex_AndUpdatesViaRepo()
+        {
+            //Arrange
+            var degree = new Degree() { DegreeId = 0 };
+
+            //Act
+            var result = await _uut.Edit(0, degree);
+            //Assert
+
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            Assert.AreEqual("Index", (result as RedirectToActionResult)?.ActionName);
+            await _subRepo.Received().UpdateDegree(degree);
+        }
+
+        [Test]
+        public async Task Delete_WithNoId_ReturnsNotFound()
+        {
+            //Arrange
+
+            //Act
+            var result = await _uut.Delete(id: null);
+            //Assert
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Delete_WithValidId_ReturnsViewWithDegree()
+        {
+            //Arrange
+            var degree = new Degree() {DegreeId = 0};
+            _subRepo.FindDegree(0).Returns(degree);
+            //Act
+            var result = await _uut.Delete(0);
+            //Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            _subRepo.Received().FindDegree(0);
+            Assert.AreEqual(degree, (result as ViewResult)?.ViewData.Model);
+        }
+
+        [Test]
+        public async Task DeleteConfirmed_WithValidId_ReturnsRedirectToIndex()
+        {
+            //Arrange
+            var degree = new Degree() { DegreeId = 0 };
+            _subRepo.FindDegree(0).Returns(degree);
+            //Act
+            var result = await _uut.DeleteConfirmed(0);
+            //Assert
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            _subRepo.Received().FindDegree(0);
+            _subRepo.Received().RemoveDegree(degree);
+            Assert.AreEqual("Index", (result as RedirectToActionResult)?.ActionName);
+        }
+
+        [Test]
+        public async Task DeleteConfirmed_WithInvalidId_ReturnsRedirectToIndex()
+        {
+            //Arrange
+            //Act
+            var result = await _uut.DeleteConfirmed(-5);
+            //Assert
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            Assert.AreEqual("Index", (result as RedirectToActionResult)?.ActionName);
+        }
 
 
     }
